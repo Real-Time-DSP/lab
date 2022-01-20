0928e799febba06534f25f052d89c7f3895caba7
Dan Jacobellis <danjacobellis@utexas.edu>
2022-01-19 18:08:30 -0600

    updated build script to make two versions

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Building HTML
+# [Real-time DSP lab manual](https://real-time-dsp.github.io/lab/)
 
-`python build.py` 
+## Organization
+
+The source materials consist of MyST markdown files (.md) which are built into a webpage using [Jupyter Book](https://jupyterbook.org/intro.html)
+
+## Contribute
+
+If you would like to be added as a contributor to this repository, contact [Dan Jacobellis](https://danjacobellis.net)
+
+## Building and hosting
+
+The [build.py](build.py) script will create two versions of the webpage.
+
+`python build.py`
+
+The first version is placed in the /docs folder where github pages automatically hosts the webpage. For links to other course materials in this version, the full ece.utexas.edu URL is used. This version will be automatically published to https://real-time-dsp.github.io/lab/ whenever changes are made.
+
+The build.sh script creates a second version of the HTML files in stm32h735gdk.zip. This version uses relative links to reference any files in the hierarchy higher than 'courses/realtime/lectures/laboratory/stm32h735gdk' (such as lecture slides, matlab info, etc).
--- build.py
+++ build.py
@@ -1,17 +1,35 @@
 def build():
     from subprocess import run
-    from shutil import rmtree, copytree
+    from shutil import rmtree, copytree, make_archive
     from os.path import exists
     from pathlib import Path
+    from re import sub
+    
+    # Build the lab manual
     result = run("jupyter-book clean -a .",capture_output=True)
     print(result.stdout.decode("utf-8"))
     result = run("jupyter-book build --builder html .",capture_output=True)
     print(result.stdout.decode("utf-8"))
+    
+    # Copy to docs folder to publish to github pages
     if exists("docs"):
         rmtree("docs")
     copytree("_build/html","docs")
     Path("docs/.nojekyll").touch()
     copytree("_static","docs/_static",dirs_exist_ok=True)
+    
+    #Make another copy in the stm32h735gdk folder for utexas.edu webpage
+    if exists("stm32h735gdk"):
+        rmtree("stm32h735gdk")
+    for path in Path("_build").rglob("*.html"):      
+        with open(path, "r", encoding="utf-8") as htmlfile:
+            lines = htmlfile.readlines()
+        with open(path, "w", encoding="utf-8") as htmlfile:
+            for line in lines:
+                htmlfile.write(sub("http://users.ece.utexas.edu/~bevans/courses/realtime/", "../../../../", line))
+    copytree("_build/html","stm32h735gdk")
+    copytree("_static","stm32h735gdk/_static",dirs_exist_ok=True)
+    make_archive('stm32h735gdk', 'zip', 'stm32h735gdk')
     
 if __name__ == '__main__':
     build()
